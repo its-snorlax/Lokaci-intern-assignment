a547e581a1caf44e6aa32a22a7a63cfa4aadf3dd
Prayas Jain <p.jain3322@gmail.com>
2019-07-10 23:22:35 +0530

    remove imports and show title on map markers

--- app/src/main/java/com/example/lokaciinternassignment/activity/MapsActivity.java
+++ app/src/main/java/com/example/lokaciinternassignment/activity/MapsActivity.java
@@ -2,9 +2,6 @@ package com.example.lokaciinternassignment.activity;
 
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Picture;
-import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
@@ -42,22 +39,13 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
             mapFragment.getMapAsync(this);
         }
 
-        shopListItems = new ArrayList<>();
-        shopListItems.add(new ShopListItem(R.drawable.shop_one, "New Delhi", 5.0, 2.0, "Address One", 28.6139, 77.2090));
-        shopListItems.add(new ShopListItem(R.drawable.shop_two, "Agra", 4.0, 5.0, "Address Two", 27.1767, 78.0081));
-        shopListItems.add(new ShopListItem(R.drawable.shop_three, "Jaipur", 3.0, 7.0, "Address Three", 26.9124, 75.7873));
-        shopListItems.add(new ShopListItem(R.drawable.shop_four, "Gurugram", 2.0, 1.0, "Address Four", 28.4595, 77.0266));
-        shopListItems.add(new ShopListItem((R.drawable.red_kitten_01), "Apka Bazar", 3.0, 2.5, "Address Five", 28.4598, 77.0312));
-
-
         RecyclerView recyclerView = findViewById(R.id.recycle_view);
-        ShopListAdapter shopListAdapter = new ShopListAdapter(shopListItems);
+        final ShopListAdapter shopListAdapter = new ShopListAdapter(getList());
         final PagerSnapHelper pagerSnapHelper = new PagerSnapHelper();
 
         recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
         recyclerView.setAdapter(shopListAdapter);
         pagerSnapHelper.attachToRecyclerView(recyclerView);
-
         recyclerView.addOnScrollListener(new SnapPositionDetector(this, pagerSnapHelper));
     }
 
@@ -71,7 +59,7 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
             String shopName = shopListItem.getShopName();
             double rating = shopListItem.getRating();
             LatLng coordinate = new LatLng(latitude, longitude);
-            googleMap.addMarker(new MarkerOptions().position(coordinate).title(shopName).icon(BitmapDescriptorFactory.fromBitmap(createBitmap(shopName,rating))));
+            googleMap.addMarker(new MarkerOptions().position(coordinate).title(shopName).icon(BitmapDescriptorFactory.fromBitmap(createBitmap(shopName, rating))));
         }
     }
 
@@ -79,14 +67,15 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     public void onSnapPositionChange(int position) {
         ShopListItem currentItem = shopListItems.get(position);
         LatLng coordinate = new LatLng(currentItem.getLatitude(), currentItem.getLongitude());
-        MarkerOptions markerOptions = new MarkerOptions().position(coordinate).title(currentItem.getShopName()).icon(BitmapDescriptorFactory.fromBitmap(createBitmap(currentItem.getShopName(),currentItem.getRating())));
-        googleMap.addMarker(markerOptions).showInfoWindow();
+
+        final Marker currentMarker = googleMap.addMarker(new MarkerOptions().position(coordinate).title(currentItem.getShopName()).icon(BitmapDescriptorFactory.fromBitmap(createBitmap(currentItem.getShopName(), currentItem.getRating()))));
+        currentMarker.showInfoWindow();
         googleMap.moveCamera(CameraUpdateFactory.newLatLng(coordinate));
         googleMap.animateCamera(CameraUpdateFactory.zoomIn());
         googleMap.animateCamera(CameraUpdateFactory.zoomTo(15F), 2000, null);
     }
 
-    private Bitmap createBitmap(String name,Double rating) {
+    private Bitmap createBitmap(String name, Double rating) {
         View markerLayout = getLayoutInflater().inflate(R.layout.custom_marker, null);
 
         TextView nameView = markerLayout.findViewById(R.id.name);
@@ -102,5 +91,15 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         Canvas canvas = new Canvas(bitmap);
         markerLayout.draw(canvas);
         return bitmap;
+    }
+
+    private ArrayList<ShopListItem> getList() {
+        shopListItems = new ArrayList<>();
+        shopListItems.add(new ShopListItem(R.drawable.shop_one, "New Delhi", 5.0, 2.0, "Address One", 28.6139, 77.2090));
+        shopListItems.add(new ShopListItem(R.drawable.shop_two, "Agra", 4.0, 5.0, "Address Two", 27.1767, 78.0081));
+        shopListItems.add(new ShopListItem(R.drawable.shop_three, "Jaipur", 3.0, 7.0, "Address Three", 26.9124, 75.7873));
+        shopListItems.add(new ShopListItem(R.drawable.shop_four, "Gurugram", 2.0, 1.0, "Address Four", 28.4595, 77.0266));
+        shopListItems.add(new ShopListItem((R.drawable.red_kitten_01), "Apka Bazar", 3.0, 2.5, "Address Five", 28.4598, 77.0312));
+        return shopListItems;
     }
 }
